2f16d1232e974ed066c39b659aba067a63e46a78
yuzub <yuriy.zubaha@gmail.com>
2016-05-05 01:44:07 +0300

    Added files via upload

--- script.js
+++ script.js
@@ -9,22 +9,39 @@ let obj = {
   arr: ["some", "array", {someProp: "value"}],
   prop: { key: 1 },
   empty: null,
-  last: 0
+  last: 0,
+  num1: 75.757575
 };
 
-console.log(obj);
-let obj2 = dz1obj(obj);
-for (let prop in obj2) {
-	console.log(obj2[prop]);
-};
+function dz1() {
+	let pRes1 = document.getElementById("res1");
+	console.log(obj);
+	let obj2 = dz1obj(obj);
+	for (let prop in obj2) {
+		console.log(obj2[prop]);
+		pRes1.innerHTML += "<br/>" + obj2[prop];
+	};
+	console.log(obj);
+	pRes1.innerHTML += "<br/>" + obj;
+	}
 
 function dz1obj(ob) {
 	let objResult = {};
 
 	let objKeys = Object.keys(ob);
 	objResult["количество собственных свойств"] = objKeys.length;
-	// objResult.propTypes = ;
+	objResult.propTypes = [];
 	objResult.propNames = objKeys;
-	
+	for (let prop in ob) {
+			let type = typeof obj[prop]
+			objResult.propTypes.push(type);
+			if (type === "number") {
+				obj[prop] = +obj[prop].toFixed(2);
+			}
+			if (type === "string") {
+				obj[prop] = obj[prop].toUpperCase();
+			}
+
+		}	
 	return objResult;
 }
